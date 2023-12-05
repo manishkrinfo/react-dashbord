ad68b3b382b1f520f72aca5823bf23386a172d49
MANISH KUMAR <mkrinfo1@gmail.com>
2023-12-05 14:02:53 +0530

    create small & large widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,18 @@ import './home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import Chart from '../../components/chart/Chart';
 import {userData} from '../../dummyData'
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid datakey="Active User"  />
+      <div className='homeWidgets'>
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   )
 }
