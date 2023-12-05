cee8a8b7e19f0a421e799c9dfcc6ca03ae46784a
MANISH KUMAR <mkrinfo1@gmail.com>
2023-12-05 17:12:27 +0530

    create user component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import './app.css';
+import { Route,  BrowserRouter as Router, Routes} from "react-router-dom";
 import Home from './pages/home/Home';
+import UserList from "./pages/userList/UserList";
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
-      <div className='container'>
+      <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/users" element={<UserList/>} />
+          <Route path="/user/:userId" element={<User/>} />
+          <Route path="/newUser" element={<NewUser/>} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
